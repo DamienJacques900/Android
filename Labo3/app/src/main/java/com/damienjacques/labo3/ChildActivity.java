2736de9fb683aee9496d821d3a1e6c504992c2c8
DamienJacques900 <damien.jacques900@gmail.com>
2016-10-17 20:33:25 +0200

    gerant

--- Labo3/app/src/main/java/com/damienjacques/labo3/ChildActivity.java
+++ Labo3/app/src/main/java/com/damienjacques/labo3/ChildActivity.java
@@ -2,7 +2,7 @@ package com.damienjacques.labo3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.util.Log;
 
 
 public class ChildActivity extends AppCompatActivity
@@ -12,4 +12,41 @@ public class ChildActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
     }
+    /*
+    @Override
+    public void onStart()
+    {
+        Log.i("lifeCycle","onStart est exécuter");
+    }
+
+    @Override
+    public void onResume()
+    {
+        Log.i("lifeCycle","onResume est exécuter");
+    }
+
+    @Override
+    public void onPause()
+    {
+        Log.i("lifeCycle","onPause est exécuter");
+    }
+
+    @Override
+    public void onStop()
+    {
+        Log.i("lifeCycle","onStop est exécuter");
+    }
+
+    @Override
+    public void onRestart()
+    {
+        Log.i("lifeCycle","onRestart est exécuter");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Log.i("lifeCycle","onDestroy est exécuter");
+    }
+    */
 }
--- Labo3/app/src/main/java/com/damienjacques/labo3/MainActivity.java
+++ Labo3/app/src/main/java/com/damienjacques/labo3/MainActivity.java
@@ -3,6 +3,7 @@ package com.damienjacques.labo3;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -51,6 +52,40 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*@Override
+    public void onStart()
+    {
+        Log.i("lifeCycle","onStart est exécuter");
+    }
 
+    @Override
+    public void onResume()
+    {
+        Log.i("lifeCycle","onResume est exécuter");
+    }
 
+    @Override
+    public void onPause()
+    {
+        Log.i("lifeCycle","onPause est exécuter");
+    }
+
+    @Override
+    public void onStop()
+    {
+        Log.i("lifeCycle","onStop est exécuter");
+    }
+
+    @Override
+    public void onRestart()
+    {
+        Log.i("lifeCycle","onRestart est exécuter");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Log.i("lifeCycle","onDestroy est exécuter");
+    }
+    */
 }
